ebd08d22552567a061576e85d420c887eb1a7a3d
Copperfie1d <737509514@qq.com>
2018-05-25 18:20:07 +0800

    20180525

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,27 @@
+from django import template
+from ..models import Post, Category, Tag
+from django.db.models.aggregates import Count
+from blog.models import Category
+
+register = template.Library()
+
+#最新文章模板标签
+@register.simple_tag
+def get_recent_posts(num=5):
+    return Post.objects.all().order_by('-created_time')[:num]
+
+#归档模板标签
+@register.simple_tag
+def archives():
+    return Post.objects.dates('created_time', 'month', order='DESC')
+
+#分类模板标签
+@register.simple_tag
+def get_categories():
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+#标签云模板标签
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
